824b8fec388a5cab34e66d14561754cd7e898c55
Tomohiro Hizaki <magatamaclub@hisakitomohironoMacBook-Pro.local>
2019-03-31 00:21:54 +0900

    修正1

--- src/js/_ani.js
+++ src/js/_ani.js
@@ -10,41 +10,6 @@ const Ani = () => {
         return;
     }
 
-
-    if ( !window.IntersectionObserver ) {
-
-        // InterSectionObserver未対応ブラウザ
-        const getScrollTop = () => {
-            return (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        }
-
-        window.addEventListener('load', () => {
-
-            const TARGET_POSITION = target.getBoundingClientRect().bottom;
-
-            const handleScroll = (e) => {
-
-                if ( getScrollTop() > TARGET_POSITION ) {
-                    if ( !HasClass( $button, 'on') ) {
-                        AddClass( $button, 'on');
-                    }
-                    return;
-                }
-
-                if ( HasClass( $button, 'on') ) {
-                    RemoveClass( $button, 'on');
-                }   
-
-            }
-
-            window.addEventListener('scroll', handleScroll, false);
-
-        });
-
-        return;
-    }
-
-
     const callback = (entries) => {
         entries.forEach(entry => {
             
@@ -64,7 +29,7 @@ const Ani = () => {
         threshold: [ 0.3 ]
     });
 
-    targets.forEach( target => observer.observe(target) );
+    targets.forEach( target => observer.observe(target));
     markers.forEach( marker => observer.observe(marker));
 
 }
